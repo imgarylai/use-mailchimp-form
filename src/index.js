18233fe8fcc33ea7ff57ffde6e4ae5ddf60f33c0
Gary Lai <garylai1990@gmail.com>
2020-05-09 01:04:55 +0800

    feat(status): change the status description
    use loading, error and success to get the current status

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import queryString from "query-string";
 
 export const useMailChimpForm = (url) => {
   const initStatusState = {
-    isSending: false,
-    isError: false,
-    isSuccess: false,
+    loading: false,
+    error: false,
+    success: false,
   };
   const [status, setStatus] = useState(initStatusState);
   const [message, setMessage] = useState("");
@@ -15,17 +15,17 @@ export const useMailChimpForm = (url) => {
     event.preventDefault();
     const query = queryString.stringify(params);
     const endpoint = url.replace("/post?", "/post-json?") + "&" + query;
-    setStatus({ ...status, isSending: true });
+    setStatus({ ...status, loading: true });
     setMessage("sending");
     jsonp(endpoint, { param: "c" }, (error, data) => {
       if (error) {
-        setStatus({ ...initStatusState, isError: true });
+        setStatus({ ...initStatusState, error: true });
         setMessage(error);
       } else if (data.result !== "success") {
-        setStatus({ ...initStatusState, isError: true });
+        setStatus({ ...initStatusState, error: true });
         setMessage(data.msg);
       } else {
-        setStatus({ ...initStatusState, isSuccess: true });
+        setStatus({ ...initStatusState, success: true });
         setMessage(data.msg);
       }
     });
