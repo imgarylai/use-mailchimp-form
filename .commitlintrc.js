d7310a65a35cfa696ff7ec4920bd5355de93bebb
Gary Lai <garylai1990@gmail.com>
2021-12-29 10:46:24 -0800

    refactor: use commit lint rc as config file

---
+++ .commitlintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-line-length": [1, "always", 100],
+    "footer-max-line-length": [1, "always", 100],
+  },
+};
